62b4a8ea01382d5f68774c48985fbf9e979c2a53
Zoheb Boga <zohebboga@hotmail.com>
2021-09-06 15:32:48 -0400

    Added dynamic Links to index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/Date'
 import Layout, { siteTitle } from '../components/Layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
@@ -17,11 +19,13 @@ const Home = ({allPostsData}) => {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
+              <Link href={`/[psts/${id}]`}>
+                <a>{title}</a>
+              </Link>
+              <br/>
+              <small className={utilStyles.lightText}>
+                <Date dateStr={date}/>
+              </small>
             </li>
           ))}
         </ul>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,15 +1,23 @@
+import utilStyles from '../../styles/utils.module.css'
 import Layout from '../../components/Layout'
+import Date from '../../components/Date'
+import Head from 'next/head'
 import {getAllPostIds, getPostData} from '../../lib/posts'
 
+
 const Post = ({postData}) => {
-  console.log(postData)
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateStr={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+      </article>
     </Layout>
   )
 }
@@ -25,7 +33,7 @@ export async function getStaticPaths(){
 }
 
 export async function getStaticProps({params}){
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
